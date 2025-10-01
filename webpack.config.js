55be418e853c22c11570396952eb0e3b8a767f56
Chinaka-Fortune <fortchimez@gmail.com>
2025-10-01 17:13:12 +0100

    Updated the frontend with render.yaml and package installtion

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'build'),  // Changed from 'dist'
+    filename: 'static/js/bundle.js',  // Put in static/js folder
+    publicPath: '/',  // Changed from '/dist/'
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',  // Your HTML template
+      filename: 'index.html'
+    })
+  ],
   module: {
     rules: [
       {
@@ -21,6 +28,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ],
   },
   optimization: {
